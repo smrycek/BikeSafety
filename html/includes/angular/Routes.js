6040d7a9b59066448020a785f1add43aee2107aa
Dane Summers <dsummersl@yahoo.com>
2015-02-03 22:27:10 -0500

    Adds accidents to map.
    Also works toward re-doing the accident severity calculation.

--- html/includes/angular/Routes.js
+++ html/includes/angular/Routes.js
@@ -36,11 +36,11 @@ OCEM.config(['$httpProvider', function ($httpProvider) {
 function indexCtrl($scope, $http, $q) {
     $scope.clickedFeature = "";
     $scope.marker = false;
-    $scope.url = '/data/durham-bike-lanes.topojson';
-    var addedListener = false;
     var wasLoaded = false;
 
     $('#pleaseWaitDialog').modal('show');
+    $('#modalStatus').text("Loading Google Maps...");
+
     var routeInfo = $('#route_info');
     routeInfo.dialog({ autoOpen: false, position: { my: "right bottom", at: "right-14 bottom-28", of: ".angular-google-map-container" } }); // Initialize dialog plugin
 
@@ -48,131 +48,129 @@ function indexCtrl($scope, $http, $q) {
         center: {latitude: 35.9886, longitude: -78.9072},
         zoom: 12,
         options: {
-          minZoom: 12
+            minZoom: 12
         },
         events: {
             tilesloaded: function (map) {
-                $scope.mapInstance = map;
-                if (!addedListener) {
-                    map.data.addListener('addfeature', function (event) {
-                        event.feature.setProperty("severity", "0");
-                        event.feature.setProperty("severityCount", "0");
-                        event.feature.wrecks = [];
-                    });
+                if (wasLoaded) {
+                  return;
                 }
-                if (!wasLoaded) {
-                    $('#modalStatus').text("Loading Bike Routes...");
-                    $scope.dataSet = [];
+                $('#modalStatus').text("Loading Bike Routes...");
+                $scope.dataSet = [];
+                var dataset = null;
+                var roads = null;
 
-                    $http.get($scope.url).
-                        then(function(result) {
-                            $('#modalStatus').text("Retrieving & Processing Crash Data...");
-                            var roads = topojson.feature(result.data,result.data.objects['durham-bike-lanes']).features;
-                            var overlay = new google.maps.OverlayView();
-                            overlay.onAdd = function() {
-                                var svg = d3.select(this.getPanes().overlayLayer)
-                                    .append('div')
-                                        .attr('class','bikeOverlay')
-                                    .append('svg')
-                                        .attr('class','bikeSvg');
-                                var roadGroup = svg.append('g')
-                                    .attr('class','roads');
-
-                                overlay.draw = function() {
-                                    var overlayProjection = this.getProjection();
-
-                                    var d3Projection = function(gmapCoord) {
-                                        var googleCoordinates = new google.maps.LatLng(gmapCoord[1], gmapCoord[0]);
-                                        var pixelCoordinates = overlayProjection.fromLatLngToDivPixel(googleCoordinates);
-                                        return [pixelCoordinates.x + 4000 , pixelCoordinates.y + 4000];
-                                    };
-
-                                    var path = d3.geo.path().projection(d3Projection);
-                                    roadGroup.selectAll('path')
-                                        .data(roads)
-                                        .attr('d',path)
-                                        .enter().append('svg:path')
-                                        .attr('d',path)
-                                        .attr('class','road');
-                                };
-                            };
-                            overlay.setMap(map);
-                            return $http.get("/data/durham.json");
-                        }).
-                        then(function(result) {
-                            $scope.dataSet = [];
-                            $scope.highestWrecks = 0;
-                            result.data.forEach(function(item) {
-                                if(item.city !== 'Durham') { return; }
-                                map.data.forEach(function(feature) {
-                                    var featureFound = false;
-                                    feature.getGeometry().getArray().forEach(function(coord) {
-                                        var dist = calcCrow(coord.lat(), coord.lng(), item.location.latitude, item.location.longitude);
-                                        if (dist < 0.04572 && !featureFound) { //150 feet
-                                            featureFound = true;
-                                            feature.setProperty("severityCount", parseFloat(feature.getProperty('severityCount'))+1);
-                                            if (parseFloat(feature.getProperty('severityCount')) > $scope.highestWrecks) {
-                                                $scope.highestWrecks = parseFloat(feature.getProperty('severityCount'));
-                                                $scope.highestWreckLoc = item;
-                                            }
-                                            feature.wrecks.push(item);
-                                            $scope.dataSet.push(item);
-                                        }
-                                    });
-                                });
-                            });
-                            map.data.forEach(function(feature) {
-                                feature.setProperty("severity", parseFloat((feature.getProperty('severityCount'))/$scope.highestWrecks));
-                            });
-
-                            map.data.addListener('click', function(event){
-                                $scope.clickedFeature = event.feature;
-                                var routeInfo = $('#route_info');
-                                $('.ui-dialog-titlebar-close').html("X");
-                                $('#wreck_count').text(event.feature.getProperty('severityCount'));
-                                routeInfo.dialog( "open" ); // Initialize dialog plugin
-                                $scope.$apply();
-                                routeInfo.dialog( "option", "position", { my: "right bottom", at: "right-14 bottom-28", of: ".angular-google-map-container" } );
-                            });
-                            $('#pleaseWaitDialog').modal('hide');
-                        }).
-                        catch(function(err) {
-                          console.error(err);
+                $('#modalStatus').text("Retrieving & Processing Crash Data...");
+                $http.get('/data/durham.json').then(function(result) {
+                    dataset = result.data;
+                    return $http.get("/data/durham-bike-lanes.topojson");
+                }).then(function(result) {
+                    roads = topojson.feature(result.data,result.data.objects['durham-bike-lanes']).features;
+                }).then(function() {
+                    var path = d3.geo.path();
+                    $scope.highestWrecks = 0;
+                    roads.forEach(function(road) {
+                        // TODO instead use the topojson data that D3 is using.
+                        //
+                        // The data here should be thought of as pre-computed,
+                        // because it will be. When it is pre-computed.
+                        //
+                        // There should be a set of two points - those that are
+                        // the road (with color/value information), and then the
+                        // points that weren't included (too far away). Those
+                        // are interesting too in that they communicate where a
+                        // bike path is lacking - where there SHOULD be a bike
+                        // path (bike path desert).
+                        var c = path.centroid(roads[0]);
+                        // console.log("|dataset[0] = "+ JSON.stringify(dataset[0]));
+                        console.log("|d = "+ d3.geo.distance(c,[dataset[0].longitude,dataset[0].latitude]));
+                        [].forEach(function(feature) {
+                            // var featureFound = false;
+                            // feature.getGeometry().getArray().forEach(function(coord) {
+                            //     var dist = calcCrow(coord.lat(), coord.lng(), item.location.latitude, item.location.longitude);
+                            //     if (dist < 0.04572 && !featureFound) { //150 feet
+                            //         featureFound = true;
+                            //         feature.setProperty("severityCount", parseFloat(feature.getProperty('severityCount'))+1);
+                            //         if (parseFloat(feature.getProperty('severityCount')) > $scope.highestWrecks) {
+                            //             $scope.highestWrecks = parseFloat(feature.getProperty('severityCount'));
+                            //             $scope.highestWreckLoc = item;
+                            //         }
+                            //         feature.wrecks.push(item);
+                            //         $scope.dataSet.push(item);
+                            //     }
+                            // });
                         });
+                    });
+                    map.data.forEach(function(feature) {
+                        feature.setProperty("severity", parseFloat((feature.getProperty('severityCount'))/$scope.highestWrecks));
+                    });
 
+                    map.data.addListener('click', function(event){
+                        $scope.clickedFeature = event.feature;
+                        var routeInfo = $('#route_info');
+                        $('.ui-dialog-titlebar-close').html("X");
+                        $('#wreck_count').text(event.feature.getProperty('severityCount'));
+                        routeInfo.dialog( "open" ); // Initialize dialog plugin
+                        $scope.$apply();
+                        routeInfo.dialog( "option", "position", { my: "right bottom", at: "right-14 bottom-28", of: ".angular-google-map-container" } );
+                    });
+                }).then(function() {
+                    var overlay = new google.maps.OverlayView();
+                    // TODO as you zoom in, the stroke should grow a
+                    // bit.
+                    overlay.onAdd = function() {
+                        var svg = d3.select(this.getPanes().overlayLayer)
+                            .append('div')
+                                .attr('class','bikeOverlay')
+                            .append('svg')
+                                .attr('class','bikeSvg');
+                        var roadGroup = svg.append('g')
+                            .attr('class','roads');
+                        var accidentGroup = svg.append('g')
+                            .attr('class','accidents');
+
+                        overlay.draw = function() {
+                            var overlayProjection = this.getProjection();
+
+                            var d3Projection = function(gmapCoord) {
+                                var googleCoordinates = new google.maps.LatLng(gmapCoord[1], gmapCoord[0]);
+                                var pixelCoordinates = overlayProjection.fromLatLngToDivPixel(googleCoordinates);
+                                return [pixelCoordinates.x + 4000 , pixelCoordinates.y + 4000];
+                            };
+                            var path = d3.geo.path().projection(d3Projection);
+
+                            roadGroup.selectAll('path')
+                                .data(roads, function(d) { return d.id; })
+                                .attr('d',path)
+                                .enter().append('svg:path')
+                                .attr('d',path)
+                                .attr('class','road');
+
+                            var eachCircle = function(d) {
+                                var p = d3Projection([d.longitude, d.latitude]);
+                                var s = d3.select(this);
+                                s.attr('cx', p[0]);
+                                s.attr('cy', p[1]);
+                            };
+                            accidentGroup.selectAll('circle')
+                                .data(dataset, function(d) { return d.objectid; })
+                                .each(eachCircle)
+                                .enter().append('svg:circle')
+                                .each(eachCircle)
+                                .attr('r',5)
+                                .attr('class','accident');
+                        };
+                    };
+                    overlay.setMap(map);
+                }).then(function() {
                     wasLoaded = true;
-                }
-                setInterval(updateMap($scope.mapInstance), 5000);
+                    $('#pleaseWaitDialog').modal('hide');
+                }).catch(function(err) {
+                    console.error(err);
+                });
             }
         }
-    }
-    $('#modalStatus').text("Loading Google Maps...");
-}
-
-function updateMap(mapInstance) {
-    mapInstance.data.setStyle(function(feature) {
-        if (feature.getProperty('severityCount') && parseFloat(feature.getProperty('severityCount')) == 0) {
-            color = "green";
-        } else if (feature.getProperty('severityCount')) {
-            color = getColor(parseFloat(feature.getProperty('severity')));
-        }
-        return /** @type {google.maps.Data.StyleOptions} */({
-            fillColor: color,
-            strokeColor: color,
-            strokeWeight: 3
-        });
-    });
-
-    mapInstance.set('styles', [
-        {
-            featureType: 'road',
-            elementType: 'geometry',
-            stylers: [
-                { color: '#CCCCCC' },
-                { weight: .2 }
-            ]
-        }
-    ]);
+    };
 }
 
 function getColor(value) {
@@ -182,24 +180,3 @@ function getColor(value) {
         hue = 0;
     return ["hsl(",hue,",100%,50%)"].join("");
 }
-
-//This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
-function calcCrow(lat1, lon1, lat2, lon2) {
-    var R = 6371; // km
-    var dLat = toRad(lat2-lat1);
-    var dLon = toRad(lon2-lon1);
-    var lat1 = toRad(lat1);
-    var lat2 = toRad(lat2);
-
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var d = R * c;
-    return d;
-}
-
-// Converts numeric degrees to radians
-function toRad(Value) {
-    return Value * Math.PI / 180;
-}
-
